a9b9ece1c8f886d91af76e3b1fe952fce03049d7
egorvania1 <egorvania1@gmail.com>
2024-10-05 16:24:15 +0500

    Slime's default direction can be changed

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 const SPEED = 60.0
-var direction = 1
+@export var direction = 1
 
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	if direction == -1:
+		animated_sprite.flip_h = true
 
 func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
