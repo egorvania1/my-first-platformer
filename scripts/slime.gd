81fa7e921cc87f27f92c38957e2b11f04d7aaeaa
egorvania1 <egorvania1@gmail.com>
2024-10-05 16:14:24 +0500

    Slime is CharacterBody2D

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,17 +1,20 @@
-extends Node2D
+extends CharacterBody2D
 
-const SPEED = 60
+const SPEED = 60.0
 var direction = 1
 
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func _process(delta) -> void:
+func _physics_process(delta: float) -> void:
+	velocity += get_gravity() * delta
+		
 	if ray_cast_right.is_colliding():
 		direction = -1
 		animated_sprite.flip_h = true
 	if ray_cast_left.is_colliding():
 		direction = 1
 		animated_sprite.flip_h = false
-	position.x += direction * SPEED * delta 
+	velocity.x = direction * SPEED
+	move_and_slide()
