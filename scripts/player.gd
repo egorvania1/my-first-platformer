7569a79e15e8fbb1b0979680f773427f57df403b
egorvania1 <egorvania1@gmail.com>
2024-10-12 22:06:09 +0500

    Variable jump height
    also changed jump_velocity

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,27 +2,37 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -320.0
 var jump_count = 0
-@export var max_jumps = 2
+@export var max_jumps = 1
+var is_jumping: bool = false
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var coyote_timer: Timer = $CoyoteTimer
 
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	#reset double jumps and jump check
 	else:
-		jump_count = 0
+		is_jumping = false #player is definetely not in jump
+		jump_count = 0 #recharge jumps
+	
+	#start falling if jump button released
+	if Input.is_action_just_released("jump") and velocity.y < 0 and not is_on_floor() and is_jumping:
+		coyote_timer.stop() #stop coyote timer to prevent accidental jumps
+		velocity.y = 0
+	#TODO: make funciton for stopping jump prettier
 		
-	var was_on_floor = is_on_floor()
-
 	# Handle jump.
+	# jump if we have jumps or coyote timer is going
 	if Input.is_action_just_pressed("jump") and ((jump_count < max_jumps) or !coyote_timer.is_stopped()):
+		is_jumping = true #player initiated jump
 		velocity.y = JUMP_VELOCITY
-		jump_count += 1
+		jump_count += 1 #take one jump
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -48,8 +58,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
 	
+	#check if character was on floor for coyote jump timer
+	#is_on_floor() updates only after move_and_slide()
+	var was_on_floor = is_on_floor()
+	
+	move_and_slide() #MOVE!!!
+	
+	#start coyote jump if not on floor
 	if was_on_floor && !is_on_floor():
 		coyote_timer.start()
