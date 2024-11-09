0305b9b6488bb70150723a2151e50eb312e74541
egorvania1 <egorvania1@gmail.com>
2024-11-09 17:56:51 +0500

    Coin pickup sound no longer overlapping

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,7 +2,11 @@ extends Area2D
 
 @onready var game_manager: Node = %GameManager
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var pickup_sound = $PickupSound
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
+	if pickup_sound.is_playing():
+		pickup_sound.stop()
+	pickup_sound.play()
 	animation_player.play("pickup")
